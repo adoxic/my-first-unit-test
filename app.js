0471a34ba28f6051736d4895fb87b0232f59aaed
Maeve Griffin <griffinmaeve@gmail.com>
2019-07-11 19:37:50 -0700

    It's working remember to add type module

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+import add from './add.js';
+
 //Num inputs x and y
 const numIn1 = document.getElementById('x');
-const numIn1 = document.getElementById('y');
+const numIn2 = document.getElementById('y');
 //Sum of numbers
 const numSum = document.getElementById('sum')
+
+//Wire up events
+numIn1.oninput = showSum;
+numIn2.oninput = showSum;
+
+function showSum() {
+    const x = Number(numIn1.value);
+    const y = Number(numIn2.value);
+    
+    numSum.textContent = add(x, y);    
+}
 
